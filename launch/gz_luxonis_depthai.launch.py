46914988279e6ee8ac925fb4a85fef3f319dd116
husarafal <rafal.zelazko@husarion.com>
2025-10-07 19:32:12 +0200

    remove oak default name from luxonis_depthai gz bridge launch

--- launch/gz_luxonis_depthai.launch.py
+++ launch/gz_luxonis_depthai.launch.py
@@ -39,10 +39,6 @@ def generate_launch_description():
     component_name = LaunchConfiguration("component_name")
     gz_bridge_name = LaunchConfiguration("gz_bridge_name")
 
-    component_name = PythonExpression(
-        ["'", component_name, "' if '", component_name, "' else 'oak'"]
-    )
-
     namespaced_gz_bridge_config_path = ReplaceString(
         source_file=gz_bridge_config_path,
         replacements={
