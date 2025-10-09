d0ee35e03a57ef5f785e1813c9b952e79f020a47
husarafal <rafal.zelazko@husarion.com>
2025-10-09 11:11:19 +0200

    allow teltonika without name, producing "gps" frame without any prefix

--- launch/gz_components.launch.py
+++ launch/gz_components.launch.py
@@ -54,7 +54,8 @@ def get_launch_description(name: str, package: str, namespace: str, component: y
         # "slamtec_rplidar": "rplidar",
         # ^ removed for compatibility with hw driver
         "stereolabs_zed": "zed",
-        "teltonika": "gps",
+        # "teltonika": "gps",
+        # ^ removed for compatibility with hw driver
         "ur": "ur",
         # "velodyne": "velodyne",
         # ^ removed for compatibility with hw driver
